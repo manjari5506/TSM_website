68ea85fa4f2ada9f15bc2f386af8b3323e93eae8
Ajay Vishwakarma <ajayvish936@gmail.com>
2024-10-02 00:06:02 +0530

    app dev added

--- app/(site)/layout.tsx
+++ app/(site)/layout.tsx
@@ -29,7 +29,7 @@ export default function RootLayout({
           <Navbar />
           {/* <ToasterContext /> */}
           {children}
-          {/* <Footer /> */}
+          <Footer />
           <ScrollToTop />
         </ThemeProvider>
       </body>
