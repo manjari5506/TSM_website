73ebf2191ec8e1a45606084e9fcc92824d2a3b41
Lokesh002cse <lokeshkmr995@gmail.com>
2024-10-02 18:21:33 +0530

    jon opening buttons

--- app/(site)/career/page.tsx
+++ app/(site)/career/page.tsx
@@ -4,7 +4,8 @@ import './styles.css';
 // import App from '@/components/cardnavigate';
 import ApplicationForm from '@/components/Apply';
 
-import Cards from '@/components/Core';
+// import Cards from '@/components/Core';
+import CoreValues from '@/components/Overview/CoreValue';
 
 
 const Careerspage = () => {
@@ -22,10 +23,7 @@ const Careerspage = () => {
       
 <section className="hero-section relative">
    
-    <video autoPlay muted loop className='absolute bg-cover w-full z-0'>
-      {/* <source src="/videos/6916531_Motion Graphics_Motion Graphic_1920x1080 (1).mp4" /> */}
-      Your browser does not support the video tag.
-    </video>
+   
   
   <div className="hero-content relative z-1">
     <div className="hero-text">
@@ -34,78 +32,79 @@ const Careerspage = () => {
         Be part of an open and collaborative environment where freedom and opportunity thrive. Together, we're more than colleagues - we're a team!
       </p>
       <div className="button-container">
-        <button className="job-button ease-in-out hover:scale-110">Job Openings →</button>
+        <a className="job-button ease-in-out hover:scale-110 " href='#career-opening'>Job Openings →</a>
       </div>
     </div>
   </div>
 </section>
 
 
-      {/* Innovation Section */}
-      <section className="innovation-section">
-        <div className="innovation-container">
-            <h2>
-              Our <span className="ventures">Ventures</span> Work
-            </h2>
-   
-            <p>
-              StackMentalist is dedicated to delivering platform-independent, customer-centric software solutions. With extensive experience across diverse industries and business domains, our proven delivery model guarantees high-quality, cost-effective application development tailored to specific business requirements.
-            </p>
-            
-          
+<section className="innovation-section py-20  bg-white-100">
+  <div className="innovation-container">
+    <h2>Our Ventures Work</h2>
+    <p>
+      StackMentalist is dedicated to delivering platform-independent,<br/> customer-centric software solutions. 
+    </p>
+
+    {/* Cards Section */}
+    <div className="card-section">
+      {/* Card 1 */}
+      <div className="card">
+        <div className='w-full'>
+
+        <img src="/images/work/group-1.png" alt="Enjoy What You Do"  className='w-full'/>
         </div>
-      </section>
-
-      {/* Cards Section */}
-      <section className="py-40 bg-white-100">
-        <div className="  mx-auto px-[5rem] grid grid-cols-1 md:grid-cols-3 gap-8">
-          {/* Card 1 */}
-          <div className="bg-white rounded-lg shadow-lg overflow-hidden">
-            <img src="/images/work/group-1.png" alt="Enjoy What You Do" className="w-full  object-cover" />
-            <div className="p-6">
-              <h3 className="text-3xl font-bold mb-4" style={{ fontFamily: 'Playfair Display' }}>Enjoy What You Do</h3>
-              <p className="text-gray-700 font-'Kodchasan' text-base">
-                We have cultivated a culture where we carry lightness and fun into what we do. We celebrate all important occasions, have offsite trips, and conduct engaging events throughout the year. We find joy in building solutions we’re proud of.
-              </p>
-            </div>
-          </div>
-
-          {/* Card 2 */}
-          <div className="bg-white rounded-lg shadow-lg overflow-hidden">
-            <img src="/images/work/group-2.png" alt="Come Up With Ideas" className="w-full  object-cover" />
-            <div className="p-6">
-              <h3 className="text-3xl font-bold mb-4" style={{ fontFamily: 'Playfair Display'}}>Come Up With Ideas</h3>
-              <p className="text-gray-700 font-'Kodchasan' text-base">
-                We are a place that cherishes ideas. We strive to create an environment where our tribe can have clear opinions and share them without any hesitation, and at the same time be humble & open to new ideas.
-              </p>
-            </div>
-          </div>
-
-          {/* Card 3 */}
-          <div className="bg-white rounded-lg shadow-lg overflow-hidden">
-            <img src="/images/work/group-3.png" alt="A Venture in Unity" className="w-full  object-cover" />
-            <div className="p-6">
-              <h3 className="text-3xl font-bold mb-4" style={{ fontFamily: 'Playfair Display' }}>A Venture in Unity</h3>
-              <p className="text-gray-700 font-'Kodchasan' text-base">
-                We believe in creating a community where everyone feels at home, regardless of their background. Our tribe is built on the principles of inclusion, respect, and shared leadership, ensuring that each individual is heard, valued, and uplifted.
-              </p>
-            </div>
-          </div>
+        <div className="content py-2">
+          <h3>Enjoy What You Do</h3>
+          <p>
+            We have cultivated a culture where we carry lightness and fun into what we do. We celebrate all important occasions, have offsite trips, and conduct engaging events throughout the year. We find joy in building solutions we’re proud of.
+          </p>
         </div>
-      </section>
-      <section>
+      </div>
+
+      {/* Card 2 */}
+      <div className="card ">
+      <div className='w-full'>
+        <img src="/images/work/group-2.png" alt="Come Up With Ideas"  className='w-full h-[22.4rem]'/>
+        </div>
+        <div className="content">
+          <h3>Come Up With Ideas</h3>
+          <p>
+            We are a place that cherishes ideas. We strive to create an environment where our tribe can have clear opinions and share them without any hesitation, and at the same time be humble & open to new ideas.
+          </p>
+        </div>
+      </div>
+
+      {/* Card 3 */}
+      <div className="card">
+      <div className='w-full '>
+        <img src="/images/work/group-3.png" alt="A Venture in Unity" className='w-full' />
+        </div>
+        <div className="content">
+          <h3>A Venture in Unity</h3>
+          <p>
+            We believe in creating a community where everyone feels at home, regardless of their background. Our tribe is built on the principles of inclusion, respect, and shared leadership, ensuring that each individual is heard, valued, and uplifted.
+          </p>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+
+
+      {/* <section>
         
-        <Cards/>
-      </section>
+        <CoreValues/>
+      </section> */}
 
       
       
       {/* Current Openings Section */}
-      <section className="career-openings">
-        <div className='text'>
+      <section className="career-openings" id="career-opening">
+        
           
           <p className='current'>Current <span className="highlight">Openings</span></p>
-        </div>
+        
         
         <div className="job-listings">
           {/* Job Opening Cards */}
